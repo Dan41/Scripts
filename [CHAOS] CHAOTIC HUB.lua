ae5a5622b528499239ce62fd64dff2354472f0ef
Termux_404 <82978554+Dan41@users.noreply.github.com>
2024-10-19 00:01:30 -0300

    Update [CHAOS] CHAOTIC HUB.lua

--- [CHAOS] CHAOTIC HUB.lua
+++ [CHAOS] CHAOTIC HUB.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Dan41/Scripts/refs/heads/main/PRIVATE%20SOURCE%20CODE.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Dan41/Scripts/refs/heads/main/CH%20SOURCE%20PRIVATE.lua'),true))()
