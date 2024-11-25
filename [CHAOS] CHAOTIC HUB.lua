82b3beef738b61a71d7c91d9c0f399c5491325a2
Termux_404 <82978554+Dan41@users.noreply.github.com>
2024-11-25 15:31:55 -0300

    Update [CHAOS] CHAOTIC HUB.lua

--- [CHAOS] CHAOTIC HUB.lua
+++ [CHAOS] CHAOTIC HUB.lua
@@ -1,1 +1,5 @@
+print("Chaotic Hub: created by @Termux_404")
+
+wait(1)
+
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Dan41/Scripts/refs/heads/main/CH%20SOURCE%20CODE.lua'),true))()
